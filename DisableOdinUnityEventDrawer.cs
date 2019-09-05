36b2be448b4601004ad28183ef7457775a4ff710
Mohamad Narimani <sorencoder@gmail.com>
2019-09-06 01:45:16 +0430

    Integration with Odin Inspector. Disables Odin Inspector on Unity Events.

---
+++ DisableOdinUnityEventDrawer.cs
@@ -0,0 +1,26 @@
+﻿#if ODIN_INSPECTOR
+using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector.Editor.Drawers;
+using UnityEngine.Events;
+
+namespace Merlin
+{
+    public class DisableOdinUnityEventDrawer<T> : OdinValueDrawer<T> where T : UnityEventBase
+    {
+        protected override void Initialize()
+        {
+            SkipWhenDrawing = true;
+
+            var drawers = this.Property.GetActiveDrawerChain();
+
+            foreach (var drawer in drawers)
+            {
+                if (drawer is UnityEventDrawer<T>)
+                {
+                    drawer.SkipWhenDrawing = true;
+                }
+            }
+        }
+    }
+} 
+#endif
